060b7ee989783f0d7705ca5fd5233f978227cd8b
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-05 14:40:13 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,7 +59,7 @@ class InteractiveRecord
       attributes <<  k.to_s
       attrubutes << v
     end
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attributes[0]} = #{attrubutes[1]}"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attributes[0]} = #{attributes[1]}"
     DB[:conn].execute(sql)
   end
 
